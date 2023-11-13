49de175241b14833dc623013ee7593ec901d1e90
David Morgan <david.r.morgan.92@gmail.com>
2023-11-13 18:48:58 +0000

    Create the Flask App

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+from flask import Flask
+
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World ... again!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
